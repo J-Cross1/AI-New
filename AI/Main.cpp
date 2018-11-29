39052ce5ee584e213de74bead7b5f7b8ef180cfd
Jamie Cross <j-cross29@hotmail.com>
2018-11-29 10:55:53 +0000

    commit

--- AI/Main.cpp
+++ AI/Main.cpp
@@ -17,7 +17,7 @@ int Yd[4] = { 0,0,0,1 };
 int main()
 {
 	int Niterations = 20;
-	int y = 0;
+	int y = 1;
 	float w1 = 0.3, w2 = -0.1;
 
 	float Theta = 0.2, alpha = 0.1;
@@ -26,14 +26,14 @@ int main()
 	{
 		int i = p % 4;
 		int x = x1[i] * w1 + x2[i] * w2;
-		sign(x, y, Theta);
+		y = step(x, y, Theta);
 	
 		int e = Yd[i] - y;
 
 		w1 = w1 + alpha * x1[i] * e;
 		w2 = w2 + alpha * x2[i] * e;
 
-		std::cout << w1 << "\t" << w2 << "\t" << Yd[i] << "\n";
+		std::cout << x1[i] << "\t" << x2[i] << "\t" << Yd[i] << "\t"  << y << "\t" << e << "e" <<  w1 << "\t" << w2 << "\n";
 	}
 	
 	system("pause");
