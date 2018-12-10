43c2906c686eba7b5dfaf4f5fe8aa280ea0d050f
Jamie Cross <j-cross29@hotmail.com>
2018-12-10 23:48:25 +0000

    commit

--- AI/Main.cpp
+++ AI/Main.cpp
@@ -10,30 +10,36 @@ float sign(float x, float y, float Theta);
 float sigma(float x, float y, float w);
 float linear(float x, float y, float Theta);
 
-int x1[4] = { 0,0,1,1 };
-int x2[4] = { 0,1,0,1 };
-int Yd[4] = { 0,0,0,1 };
+float x1[4] = { 0,0,1,1 };
+float x2[4] = { 0,1,0,1 };
+float Yd[4] = { 0,0,0,1 };
 
 int main()
 {
 	int Niterations = 20;
-	int y = 1;
+	float y = 1.0;
 	float w1 = 0.3, w2 = -0.1;
 
-	float Theta = 0.2, alpha = 0.1;
+	float Theta = 0, alpha = 0.1;
 
+	std::cout << "x1 \t" << "x2 \t" << "Yd \t" << "y \t" << " e \t" << "w1 \t" << "w2 \n";
+	//std::cout << "x \t y \n";
 	for (int p = 0; p < Niterations; p++)
 	{
 		int i = p % 4;
-		int x = x1[i] * w1 + x2[i] * w2;
+		float x = x1[i] * w1 + x2[i] * w2;
 		y = step(x, y, Theta);
 	
-		int e = Yd[i] - y;
+		float e;
+		e = Yd[i] - y;
 
 		w1 = w1 + alpha * x1[i] * e;
 		w2 = w2 + alpha * x2[i] * e;
 
-		std::cout << x1[i] << "\t" << x2[i] << "\t" << Yd[i] << "\t"  << y << "\t" << e << "e" <<  w1 << "\t" << w2 << "\n";
+		std::cout << x1[i] << "\t" << x2[i] << "\t" << Yd[i] << "\t" << y << "\t" << e << "\t" << w1 << "\t" << w2 << "\n";
+
+
+		//std::cout << x << "\t" << y << std::endl;
 	}
 	
 	system("pause");
